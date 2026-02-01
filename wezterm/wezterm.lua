63e436a59b346807a947159f868d0a4c333c6140
Rafael Klynger <rafael.klynger@gmail.com>
2026-02-01 16:48:49 -0300

    Things

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,5 +14,16 @@ config.color_scheme = "Catppuccin Frappé (Gogh)"
 
 config.window_background_opacity = 1
 config.macos_window_background_blur = 1
+config.default_prog = {
+	"zsh",
+	"-lc",
+	[[
+    if command -v tmux >/dev/null 2>&1 && [ -z "$TMUX" ]; then
+      exec tmux
+    else
+      exec zsh
+    fi
+  ]],
+}
 
 return config
